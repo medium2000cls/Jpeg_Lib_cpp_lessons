cb7af454b68958babfbf25be87e86bbbb9d2d9cd
Дмитрий <medium2000cls@yandex.ru>
2023-06-01 14:18:49 +0300

    СПРИНТ 15 Разработка библиотек и больших проектов Тема 6. Собираем по-новому. CMake 🛟 Урок 9. Археологические раскопки: подключаем LibJPEG Задание 2/2 Срок сдачи: 4.06. (жёсткий дедлайн)

--- ImgConverter/main.cpp
+++ ImgConverter/main.cpp
@@ -6,27 +6,101 @@
 #include <string_view>
 #include <iostream>
 
-using namespace std;
+using namespace std::literals;
+
+class ImageFormatInterface {
+public:
+    virtual bool SaveImage(const img_lib::Path& file, const img_lib::Image& image) const = 0;
+    virtual img_lib::Image LoadImage(const img_lib::Path& file) const = 0;
+};
+
+class PpmImageFormat : public ImageFormatInterface {
+public:
+    bool SaveImage(const img_lib::Path& file, const img_lib::Image& image) const override {
+        return img_lib::SaveJPEG(file, image);
+    }
+    img_lib::Image LoadImage(const img_lib::Path& file) const override {
+        return img_lib::LoadPPM(file);
+    }
+};
+
+class JpegImageFormat : public ImageFormatInterface {
+public:
+    bool SaveImage(const img_lib::Path& file, const img_lib::Image& image) const override {
+        return img_lib::SavePPM(file, image);
+    }
+    img_lib::Image LoadImage(const img_lib::Path& file) const override {
+        return img_lib::LoadJPEG(file);
+    }
+};
+
+enum class Format {
+    PPM,
+    JPEG,
+    UNKNOWN
+};
+
+Format GetFormatByExtension(const img_lib::Path& input_file) {
+    const std::string ext = input_file.extension().string();
+    if (ext == ".jpg"sv || ext == ".jpeg"sv) {
+        return Format::JPEG;
+    }
+    
+    if (ext == ".ppm"sv) {
+        return Format::PPM;
+    }
+    
+    return Format::UNKNOWN;
+}
+
+
+ImageFormatInterface* GetFormatInterface(const img_lib::Path& path) {
+    if (GetFormatByExtension(path) == Format::JPEG) {
+        return new JpegImageFormat();
+    }
+    if (GetFormatByExtension(path) == Format::PPM) {
+        return new PpmImageFormat();
+    }
+    return nullptr;
+}
+
 
 int main(int argc, const char** argv) {
     if (argc != 3) {
-        cerr << "Usage: "sv << argv[0] << " <in_file> <out_file>"sv << endl;
+        std::cerr << "Usage: "sv << argv[0] << " <in_file> <out_file>"sv << std::endl;
         return 1;
     }
 
     img_lib::Path in_path = argv[1];
     img_lib::Path out_path = argv[2];
-
-    img_lib::Image image = img_lib::LoadPPM(in_path);
+    
+    if (GetFormatByExtension(in_path) == Format::UNKNOWN) {
+        std::cerr << "Unknown format of the input file"sv << std::endl;
+        return 2;
+    }
+    if (GetFormatByExtension(out_path) == Format::UNKNOWN) {
+        std::cerr << "Unknown format of the output file"sv << std::endl;
+        return 3;
+    }
+    
+    ImageFormatInterface* image_format_ptr = GetFormatInterface(in_path);
+ 
+    if (!image_format_ptr) {
+        std::cerr << "Loading failed"sv << std::endl;
+        return 4;
+    }
+    
+    img_lib::Image image = image_format_ptr->LoadImage(in_path);
+    
     if (!image) {
-        cerr << "Loading failed"sv << endl;
+        std::cerr << "Loading failed"sv << std::endl;
         return 4;
     }
 
-    if (!img_lib::SaveJPEG(out_path, image)) {
-        cerr << "Saving failed"sv << endl;
+    if (!image_format_ptr->SaveImage(out_path, image)) {
+        std::cerr << "Saving failed"sv << std::endl;
         return 5;
     }
-
-    cout << "Successfully converted"sv << endl;
+    
+    std::cout << "Successfully converted"sv << std::endl;
 }
